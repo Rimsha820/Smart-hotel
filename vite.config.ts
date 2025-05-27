d9ab32b178edc15c41412fab0a1a89c340f39995
Rimsha Malik <rimsha820.malik@gmail.com>
2025-05-27 12:31:07 +0500

    add link

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Smart-hotel/', // 👈 this is required for GitHub Pages
   plugins: [react()],
-})
+});
